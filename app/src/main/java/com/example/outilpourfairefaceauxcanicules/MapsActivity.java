043006a1aa169a0ba9f8812fdb84857b2f8c2205
laurence2000 <laurence.delisle2000@gmail.com>
2022-10-31 11:49:12 -0400

    Initial commit

---
+++ app/src/main/java/com/example/outilpourfairefaceauxcanicules/MapsActivity.java
@@ -0,0 +1,62 @@
+package com.example.outilpourfairefaceauxcanicules;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.outilpourfairefaceauxcanicules.databinding.ActivityMapsBinding;
+
+import org.json.JSONObject;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Repentigny and move the camera
+        LatLng repentigny = new LatLng(45.753284, -73.440079);
+        mMap.addMarker(new MarkerOptions().position(repentigny).title("Marker in Repentigny"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(repentigny,15));
+
+        //Add layer to map  --testing git changes   to see if it works
+        //JSONObject geoJsonData = new JSONObject();// JSONObject containing the GeoJSON data
+        //GeoJsonLayer layer = new GeoJsonLayer(mMap, geoJsonData);
+        //GeoJsonLayer layer = new GeoJsonLayer(mMap, R.raw.geojson_file, context);
+        //layer.addLayerToMap();
+    }
+
+
+
+}
