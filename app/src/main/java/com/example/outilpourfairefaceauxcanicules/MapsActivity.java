83d6039f7c5f5191ffb7f51245a52e569a3d6d64
laurence2000 <laurence.delisle2000@gmail.com>
2022-11-01 11:06:52 -0400

    Ajout des filtres arbres, clim, eau sur la carte en Checkbox

--- app/src/main/java/com/example/outilpourfairefaceauxcanicules/MapsActivity.java
+++ app/src/main/java/com/example/outilpourfairefaceauxcanicules/MapsActivity.java
@@ -4,12 +4,15 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.outilpourfairefaceauxcanicules.databinding.ActivityMapsBinding;
 
@@ -43,6 +46,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
 
     /**
@@ -54,6 +59,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    List <Marker> arbres = new ArrayList<>();
+    List <Marker> clim = new ArrayList<>();
+    List <Marker> eau = new ArrayList<>();
+    List <Marker> parcs = new ArrayList<>();
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -63,19 +73,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(repentigny).title("Marker in Repentigny"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(repentigny,15));
 
-
-        addDataPoint(mMap, "data-arbres.csv", 8, 9, "Arbre", "Les arbres diminuent la temperature de lenvironnement.");
-        //addDataPoint(mMap, "data-clim.csv",13, 12, "Bâtiment climatisé", "Les parcs peuvent procurer une meilleure solution.");
-        //addDataPoint(mMap, "data-parcs.csv", 8, 7, "Parc","Les parcs sont des espaces froids.");
+        arbres = addDataPoint(mMap, "data-arbres.csv", 8, 9, "Arbre", "Les arbres diminuent la temperature de lenvironnement.");
+        clim = addDataPoint(mMap, "lieu-climatise.csv",13, 12, "Bâtiment climatisé", "Les parcs peuvent procurer une meilleure solution.");
+        parcs = addDataPoint(mMap, "parcs.csv", 8, 7, "Parc","Les parcs sont des espaces froids.");
         //addDataPoint(mMap, "data-mtl-parcs.csv", 22, 21, "Parc", "Info sur les parsc");
         //addDataPoint(mMap, "data-mtl-piscines.csv", 12, 11, "Piscine", "info sur les piscines");
         //À ajouter : lieux à éviter, soit les ilôts de chaleur
     }
 
     //General function for looping over each dataset and adding it to the map.
-    public void addDataPoint(GoogleMap map, String filename, int lat, int longi, String type, String text) {
+    public List<Marker> addDataPoint(GoogleMap map, String filename, int lat, int longi, String type, String text) {
         List<List<String>> records = new ArrayList<>();
         AssetManager assetManager = getAssets();
+        List<Marker> markers = new ArrayList<>();
+
         try {
             InputStream input = assetManager.open(filename);
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
@@ -84,13 +95,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             while ((line = reader.readLine()) != null) {
                 String[] values = line.split(",");
                 records.add(Arrays.asList(values));
-                LatLng arbres = new LatLng(Double.parseDouble(values[lat]), Double.parseDouble(values[longi]));
-                mMap.addMarker(new MarkerOptions().position(arbres).title(type).snippet(text));
+                LatLng coord = new LatLng(Double.parseDouble(values[lat]), Double.parseDouble(values[longi]));
+                Marker marker = mMap.addMarker(new MarkerOptions().position(coord).title(type).snippet(text).visible(false));
+
+                markers.add(marker);
+
                 //On pourrait ajouter l'option .icon pour modifier la couleur du marqueur pour cette classe de points
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        return markers;
+    }
+
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkbox_arbres:
+                if (checked)
+                    for (Marker a : arbres)
+                        a.setVisible(true);
+                else
+                    for (Marker a : arbres)
+                        a.setVisible(false);
+            case R.id.checkbox_eau:
+                if (checked)
+                    for (Marker e : eau)
+                        e.setVisible(true);
+                else
+                    for (Marker e : eau)
+                        e.setVisible(false);
+            case R.id.checkbox_parcs:
+                if (checked)
+                    for (Marker p : parcs)
+                        p.setVisible(true);
+                else
+                    for (Marker p : parcs)
+                        p.setVisible(false);
+            case R.id.checkbox_clim:
+                if (checked)
+                    for (Marker c : clim)
+                        c.setVisible(true);
+                else
+                    for (Marker c : clim)
+                        c.setVisible(false);
+            case R.id.checkbox_chaleur:
+                //if (checked)
+                break;
+
         }
     }
 
